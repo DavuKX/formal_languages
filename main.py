143d610c553f1a2c5040d5071b5cb51d0185c0c5
DavuKX <103606165+DavuKX@users.noreply.github.com>
2023-09-04 20:40:56 -0500

    make facade

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
-# This is a sample Python script.
+import typer
+from rich import print as rprint
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+app = typer.Typer()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.command("input_alphabets")
+def sample_func():
+    rprint("input_alphabets")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.command("union_alphabets")
+def sample_func():
+    rprint("[red bold]union_alphabets")
+
+
+@app.command("diff_alphabets")
+def sample_func():
+    rprint("[red bold]diff_alphabets")
+
+
+@app.command("intersect_alphabets")
+def sample_func():
+    rprint("[red bold]intersect_alphabets")
+
+
+@app.command("alphabet_star_lock")
+def sample_func():
+    rprint("[red bold]alphabet_star_lock")
+
+
+@app.command("generate_languages")
+def sample_func():
+    rprint("generate_languages")
+
+
+@app.command("union_languages")
+def sample_func():
+    rprint("[red bold]union_languages")
+
+
+@app.command("diff_languages")
+def sample_func():
+    rprint("[red bold]diff_languages")
+
+
+@app.command("concat_languages")
+def sample_func():
+    rprint("[red bold]concat_languages")
+
+
+@app.command("power_of_language")
+def sample_func():
+    rprint("[red bold]power_of_language")
+
+
+@app.command("invert_language")
+def sample_func():
+    rprint("[red bold]invert_language")
+
+
+@app.command("language_cardinality")
+def sample_func():
+    rprint("[red bold]language_cardinality")
+
+
+if __name__ == "__main__":
+    app()
