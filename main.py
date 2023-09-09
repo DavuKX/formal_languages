7ded87ee3347d5468fc14dedd2881ac9f9b3340b
Valentina9990 <dania.v.sarmiento@gmail.com>
2023-09-09 16:54:15 -0500

    Fixes to main

--- main.py
+++ main.py
@@ -1,74 +1,3 @@
-<<<<<<< HEAD
-import typer
-from rich import print as rprint
-
-app = typer.Typer()
-
-
-@app.command("input_alphabets")
-def sample_func():
-    rprint("input_alphabets")
-
-
-@app.command("union_alphabets")
-def sample_func():
-    rprint("[red bold]union_alphabets")
-
-
-@app.command("diff_alphabets")
-def sample_func():
-    rprint("[red bold]diff_alphabets")
-
-
-@app.command("intersect_alphabets")
-def sample_func():
-    rprint("[red bold]intersect_alphabets")
-
-
-@app.command("alphabet_star_lock")
-def sample_func():
-    rprint("[red bold]alphabet_star_lock")
-
-
-@app.command("generate_languages")
-def sample_func():
-    rprint("generate_languages")
-
-
-@app.command("union_languages")
-def sample_func():
-    rprint("[red bold]union_languages")
-
-
-@app.command("diff_languages")
-def sample_func():
-    rprint("[red bold]diff_languages")
-
-
-@app.command("concat_languages")
-def sample_func():
-    rprint("[red bold]concat_languages")
-
-
-@app.command("power_of_language")
-def sample_func():
-    rprint("[red bold]power_of_language")
-
-
-@app.command("invert_language")
-def sample_func():
-    rprint("[red bold]invert_language")
-
-
-@app.command("language_cardinality")
-def sample_func():
-    rprint("[red bold]language_cardinality")
-
-
-if __name__ == "__main__":
-    app()
-=======
-import sys
 import re
 import typer
 
@@ -109,4 +38,4 @@ def alphabet(a: str, b: str):
 
 if __name__ == "__main__":
     app()
->>>>>>> 7d555ec60566f77308e3fa1ae1f9e4a589a1fc2d
+
