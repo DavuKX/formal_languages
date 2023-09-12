3e6451bedf2cdc3a016082a857a36b944b38994e
anuarthr <anuarthrincongonzalez@gmail.com>
2023-09-11 21:59:40 -0500

    improve generated words with kleene closure

--- src/entities/alphabet.py
+++ src/entities/alphabet.py
@@ -25,11 +25,16 @@ class Alphabet(SetOperations):
 
     def generate_words_with_kleene_closure(self, words_number, max_word_length):
         generated_words = set()
-
+        is_lambda = 0
         while len(generated_words) < words_number:
             random_word_length = random.randint(1, max_word_length)
             random_word = ''.join(random.choice(list(self.values)) for _ in range(random_word_length))
-            generated_words.add(random_word)
+            if '#' in random_word:
+                if random_word == '#' and is_lambda == 0:
+                    generated_words.add(random_word)
+                    is_lambda+=1
+            else:
+                generated_words.add(random_word)
 
         return Alphabet(generated_words)
     
